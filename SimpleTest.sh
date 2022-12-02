a63c716dca70159c6b6ca729bcb7efbbcc88ce30
Domenic <domenic.bersch@yahoo.de>
2022-12-02 21:35:32 +0100

    Update SimpleTest.sh

--- SimpleTest.sh
+++ SimpleTest.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --time=24:00:00 # maximum allocated time
-#SBATCH --job-name=Trainbatch_301122 # name of the job
+#SBATCH --job-name=Trainbatch_021222 # name of the job
 #SBATCH --partition=gpu-12h # which partition the job should be scheduled on
-#SBATCH --output=./Trainbatch_301122-%j.out
-#SBATCH --error=./Trainbatch_301122cd-%j.err
+#SBATCH --output=./Trainbatch_021222-%j.out
+#SBATCH --error=./Trainbatch_021222-%j.err
 #SBATCH --gres=gpu:1
 ##SBATCH -w gpu[26-30]  Only define nodes inside that partition
 
@@ -11,8 +11,8 @@ dt=$(date '+%d/%m/%Y %H:%M:%S');
 echo $dt # debugging datetime print
 
 
-# python3 main.py -s train -m beit -d cifar10 -e 40 -b 128 -o ADAM -lr 0.01 -f False -p False -g "True" -t 'Training BEiT pretrained on Cifar10'
-# python3 main.py -s train -m beit -d cifar10 -e 40 -b 128 -o ADAM -lr 0.01 -f False -p True -g "True" -t 'Training BEiT pretrained on Cifar10'
+## python3 main.py -s train -m beit -d cifar10 -e 40 -b 128 -o ADAM -lr 0.01 -f False -p False -g "True" -t 'Training BEiT pretrained on Cifar10'
+## python3 main.py -s train -m beit -d cifar10 -e 40 -b 128 -o ADAM -lr 0.01 -f False -p True -g "True" -t 'Training BEiT pretrained on Cifar10'
 
 python3 main.py -s train -m efficientnet -d cifar10 -e 40 -b 128 -o ADAM -lr 0.01 -f False -p False -g "True" -t 'Training Efficientnet from scratch on Cifar10'
 python3 main.py -s train -m efficientnet -d cifar10 -e 40 -b 128 -o ADAM -lr 0.01 -f False -p True -g "True" -t 'Training Efficientnet pretrained on Cifar10'
@@ -22,16 +22,11 @@ python3 main.py -s train -m vit -d cifar10 -e 40 -b 128 -o ADAM -lr 0.01 -f Fals
 
 
 
+## python3 main.py -s train -m resnet50 -d cifar10 -e 40 -b 128 -o ADAM -lr 0.01 -f False -p False -g "True" -t 'Training ResNet50 from scratch on Cifar10'
+## python3 main.py -s train -m resnet50 -d cifar10 -e 40 -b 128 -o ADAM -lr 0.01 -f False -p True -g "True" -t 'Training ResNet50 pretrained on Cifar10'
 
-
-
-
-
-# python3 main.py -s train -m resnet50 -d cifar10 -e 40 -b 128 -o ADAM -lr 0.01 -f False -p False -g "True" -t 'Training ResNet50 from scratch on Cifar10'
-# python3 main.py -s train -m resnet50 -d cifar10 -e 40 -b 128 -o ADAM -lr 0.01 -f False -p True -g "True" -t 'Training ResNet50 pretrained on Cifar10'
-
-# python3 main.py -s train -m resnet101 -d cifar10 -e 40 -b 128 -o ADAM -lr 0.01 -f False -p False -g "True" -t 'Training ResNet101 from scratch on Cifar10'
-# python3 main.py -s train -m resnet101 -d cifar10 -e 40 -b 128 -o ADAM -lr 0.01 -f False -p True -g "True" -t 'Training ResNet101 pretrained on Cifar10'
+## python3 main.py -s train -m resnet101 -d cifar10 -e 40 -b 128 -o ADAM -lr 0.01 -f False -p False -g "True" -t 'Training ResNet101 from scratch on Cifar10'
+## python3 main.py -s train -m resnet101 -d cifar10 -e 40 -b 128 -o ADAM -lr 0.01 -f False -p True -g "True" -t 'Training ResNet101 pretrained on Cifar10'
 
 
 
